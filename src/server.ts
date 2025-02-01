fc759cc79b1bebe0e0fac7b004077efb8400172b
rakuishi <rakuishi@gmail.com>
2025-02-01 14:41:55 +0900

    Add query and mutation

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,69 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const todos = [
+	{
+		id: "1",
+		title: "Learn GraphQL",
+		completed: false,
+	},
+	{
+		id: "2",
+		title: "Learn TypeScript",
+		completed: false,
+	},
+];
+
 const typeDefs = gql`
+  type Todo {
+    id: ID!
+    title: String!
+    completed: Boolean!
+  }
+
   type Query {
-    message: String
+    getTodos: [Todo!]!
+  }
+
+  type Mutation {
+    addTodo(title: String!): Todo!
+    updateTodo(id: ID!, completed: Boolean!): Todo!
+    deleteTodo(id: ID!): Todo!
   }
 `;
 
 const resolvers = {
 	Query: {
-		message: () => "Hello, world!",
+		getTodos: () => todos,
+	},
+	Mutation: {
+		addTodo: (_: unknown, { title }: { title: string }) => {
+			const newTodo = {
+				id: String(todos.length + 1),
+				title,
+				completed: false,
+			};
+			todos.push(newTodo);
+			return newTodo;
+		},
+		updateTodo: (
+			_: unknown,
+			{ id, completed }: { id: string; completed: boolean },
+		) => {
+			const todo = todos.find((todo) => todo.id === id);
+			if (!todo) {
+				throw new Error(`Todo with id ${id} not found`);
+			}
+			todo.completed = completed;
+			return todo;
+		},
+		deleteTodo: (_: unknown, { id }: { id: string }) => {
+			const todo = todos.findIndex((todo) => todo.id === id);
+			if (todo === -1) {
+				throw new Error(`Todo with id ${id} not found`);
+			}
+			const deletedTodo = todos.splice(todo, 1);
+			return deletedTodo[0];
+		},
 	},
 };
 
